c6b4849462aa575eb0638cb5432e42aaeb74659a
juanBOzuna <70336894+juanBOzuna@users.noreply.github.com>
2022-02-17 23:10:29 -0500

    finish form 5 and refactor css global

---
+++ form_1/user_register.php
@@ -0,0 +1,12 @@
+<?php
+
+// foreach( $_REQUEST as $request ){
+//     echo $request;
+  
+// }
+// var_dump($_REQUEST);
+
+echo "<h1> INFO DESDE EL  FORMULARIO   </h1> ";
+
+echo "Nombre del usuario:  " . $_REQUEST['name']  ;
+?>
